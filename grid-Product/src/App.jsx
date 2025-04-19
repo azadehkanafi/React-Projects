f77243fcbe2979c1cc9bb4816c2a65069ac4242a
azadehkanafi <kanafi.azi@gmail.com>
2025-04-19 14:28:14 +0200

    change address images

--- grid-Product/src/App.jsx
+++ grid-Product/src/App.jsx
@@ -5,55 +5,55 @@ const products = [
     id: 1,
     name: "Wireless Headphones",
     price: 89.99,
-    image: "../assets/images/product1.avif"
+    image: "./assets/images/product1.avif"
   },
   {
     id: 2,
     name: "Bluetooth Speaker",
     price: 59.99,
-    image: "../assets/images/product2.jpeg"
+    image: "./assets/images/product2.jpeg"
   },
   {
     id: 3,
     name: "Smart Watch",
     price: 129.99,
-    image: "../assets/images/product3.webp"
+    image: "./assets/images/product3.webp"
   },
   {
     id: 4,
     name: "Gaming Mouse",
     price: 39.99,
-    image: "../assets/images/product4.jpg"
+    image: "./assets/images/product4.jpg"
   },
   {
     id: 5,
     name: "Mechanical Keyboard",
     price: 79.99,
-    image: "../assets/images/product5.jpg"
+    image: "./assets/images/product5.jpg"
   },
   {
     id: 6,
     name: "4K Monitor",
     price: 299.99,
-    image: "../assets/images/product6.jpeg"
+    image: "./assets/images/product6.jpeg"
   },
   {
     id: 7,
     name: "Laptop Stand",
     price: 25.00,
-    image: "../assets/images/product7.jpeg"
+    image: "./assets/images/product7.jpeg"
   },
   {
     id: 8,
     name: "USB-C Hub",
     price: 49.99,
-    image: "../assets/images/product8.jpg"
+    image: "./assets/images/product8.jpg"
   },
   {
     id: 9,
     name: "External SSD",
     price: 149.99,
-    image: "../assets/images/product9.jpeg"
+    image: "./assets/images/product9.jpeg"
   }
 ];
 
